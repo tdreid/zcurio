de55b2b1849687b0afd7158a64932dbf22ead089
tdreid <trevor.d.reid@gmail.com>
2019-07-14 22:21:09 -0400

    Implement secure bindings

--- src/zcurio.js
+++ src/zcurio.js
@@ -1,5 +1,5 @@
 ko.extenders.restricted = target => {
-  result = ko.pureComputed({
+  const result = ko.pureComputed({
     read: target,
     write: nv => {
       if (nv === '' || /[^A-Z_?\d]/g.test(nv.toUpperCase())) {
@@ -17,12 +17,12 @@ ko.extenders.restricted = target => {
   return result;
 };
 
-cipherCurioViewModel = function() {
+const cipherCurioViewModel = function() {
   const self = this;
   const cipher = 'HER>pl^VPk|1LTG2dNp+B(#O%DWY.<*Kf)By:cM+UZGW()L#zHJSpp7^l8*V3pO++RK2_9M+ztjd|5FP+&4k/p8R^FlO-*dCkF>2D(#5+Kq%;2UcXGV.zL|(G2Jfj#O+_NYz+@L9d<M+b+ZR2FBcyA64K-zlUV+^J+Op7<FBy-U+R/5tE|DYBpbTMKO2<clRJ|*5T4M.+&BFz69Sy#+N|5FBc(;8RlGFN^f524b.cV4t++yBX1*:49CE>VUZ5-+|c.3zBK(Op^.fMqG2RcT+L16C<+FlWB|)L++)WCzWcPOSHT/()p|FkdW<7tB_YOB*-Cc>MDHNpkSzZO8A|K;+';
   const alphabet = 'ABCDEFGH|JKLMNOPRSTUVWXYZ123456789plkdfycjqbtz()>^+.<-/#_@*%&;:';
 
-  observableCharacter = function() {
+  const observableCharacter = function() {
     const self = this;
 
     self.val = ko.observable("").extend({
@@ -30,7 +30,7 @@ cipherCurioViewModel = function() {
     });
   }
 
-  decode = ltr => {
+  const decode = ltr => {
     const plainLtr = self.key().map(v => v.val()).join('').charAt(alphabet.indexOf(ltr));
     if (plainLtr == '?') return ltr;
     if (plainLtr == '_') return ' ';
@@ -40,7 +40,7 @@ cipherCurioViewModel = function() {
   self.all = (d, e) => e.target.select();
   self.alphabet = alphabet.split('');
   self.error = ko.observable('');
-  handleError = msg => {
+  const handleError = msg => {
     self.error(msg);
     return '';
   };
@@ -49,12 +49,14 @@ cipherCurioViewModel = function() {
     r.val(c);
     return r;
   }));
+  self.displayKey = ko.computed(() => self.key().map(v => v.val()).join(''));
   self.plaintext = ko.computed(() => {
     self.error('');
     if (self.key().length != 63) return handleError('Key must be exactly 63 characters long.');
     if (self.key().map(v => v.val()).join('').match(/[^A-Z_?\d]/g) != null) return handleError('Key can only contain capital letters, digits, underscores (_) and question marks (?).');
     return cipher.split('').map(ltr => decode(ltr)).join('');
   });
+  self.plaintextRows = ko.computed(() => self.plaintext().match(/.{1,17}/g).map(l => l.split('').join(' ')));
 }
 
 function bindSecureModel() {
